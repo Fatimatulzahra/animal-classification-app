e14286556aed46dab31389746671b1b6bb69eeb6
Fatimah <zahranike04@gmail.com>
2024-11-28 16:08:20 -0600

    added export function to page.tsx in my_image folder

---
+++ src/app/my_image/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function MyImagePage() {
+    return (
+      <div>
+        <h1>My Images</h1>
+        <p>Here are your saved images!</p>
+      </div>
+    );
+  }
+  
